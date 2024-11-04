7cda9891a0d950dfaf3b6535ae0fb4226418ef52
Spectatori <cavdar2002@gmail.com>
2024-11-04 23:22:06 +0200

    feat: added footer

---
+++ apps/tasky/components/FooterComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="flex absolute bottom-0 h-28 w-full border-t-2 border-primary-150 justify-between bg-primary-50">
+        <div class="flex pt-3 pl-4 gap-2">
+            <img src="../public/navbarPictures/Tasky.jpg" class="w-20 h-20 p-4 self-center" style="border-radius:32px">
+            <div class="flex flex-col pl-10 ">
+                <p style="font-size:15px">Useful Information</p>
+                <p class="text-xs hover:text-primary-150 hover:cursor-pointer pt-1">Support</p>
+                <p class="text-xs hover:text-primary-150  hover:cursor-pointer">Often asked questions</p>
+            </div>
+            <div class="flex flex-col pl-10">
+                <p style="font-size:15px">About us</p>
+                <p class="text-xs hover:text-primary-150  hover:cursor-pointer pt-1">About Tasky</p>
+                <p class="text-xs hover:text-primary-150  hover:cursor-pointer">Careers</p>
+                <p class="text-xs hover:text-primary-150  hover:cursor-pointer">Privacy & Policy</p>
+            </div>
+            <div class="flex flex-col pl-10">
+                <p style="font-size:15px">Contacts</p>
+                <p class="text-xs text-neutral-500 pt-1">Monday - Friday</p>
+                <p class="text-xs text-neutral-500">08 AM - 19 PM UTC+02:00</p>
+                <p class="text-xs text-neutral-500">+359 123456789</p>
+                <p class="text-xs text-neutral-500">tasky@gmail.com</p>
+            </div>
+        </div>
+        <div class="flex pr-8 items-center gap-4">
+            <a href="google.com">
+                <img src="../public/footerPictures/linkedin.png"></a>
+            <a href="google.com">
+                <img src="../public/footerPictures/youtube.png"></a>
+            <a href="google.com">
+                <img src="../public/footerPictures/pinterest.png"></a>
+            <a href="google.com">
+                <img src="../public/footerPictures/instagram.png"></a>
+            <a href="google.com">
+                <img src="../public/footerPictures/facebook.png"></a>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+
+</script>
