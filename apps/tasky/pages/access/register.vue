c775d2086a3014f437b9111dd1f94cebc1feb75a
Spectatori <cavdar2002@gmail.com>
2024-11-04 21:41:43 +0200

    feat: added template files for login, index and register

---
+++ apps/tasky/pages/access/register.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h2>Home</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil deserunt eligendi reiciendis aliquam, quam laborum totam nesciunt velit, explicabo atque, dignissimos veniam cupiditate tempore suscipit obcaecati corporis facilis provident expedita.</p>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  
+  </script>
+  
+  <style scoped>
+  </style>
