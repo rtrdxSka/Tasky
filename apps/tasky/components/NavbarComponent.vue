d5c1d58a9a18350711cfda56d5c6b3b1f080974b
Spectatori <cavdar2002@gmail.com>
2024-11-04 21:19:47 +0200

    feat: created navbar

---
+++ apps/tasky/components/NavbarComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="bg-navColors-background flex justify-between border-b-2 border-navColors-hoveredButtonBG">
+        <div class="flex pl-10">
+            <img src="../public/navbarPictures/Tasky.jpg" class="w-20 p-3 rounded-3xl">
+            <button class="text-3xl self-center text-navColors-textDefault ">Tasky</button>
+        </div>
+        <div class="text-lg py-3 pr-4 text-navColors-textDefault">
+            <button class="p-4 hover:text-navColors-hoveredButtonText hover:bg-navColors-hoveredButtonBG hover:rounded-xl">
+                Features
+            </button>
+            <button class="p-4 hover:text-navColors-hoveredButtonText hover:bg-navColors-hoveredButtonBG hover:rounded-xl">
+                Getting started
+            </button>
+            <button class="p-4 hover:text-navColors-hoveredButtonText hover:bg-navColors-hoveredButtonBG hover:rounded-xl">
+                About
+            </button>
+            <button class="p-4  hover:text-navColors-hoveredButtonText hover:bg-navColors-hoveredButtonBG hover:rounded-xl">
+                Login
+            </button>
+        </div>
+    </div>
+</template>
+
+<style setup>
+
+</style>
