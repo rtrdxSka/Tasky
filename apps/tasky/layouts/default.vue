d77dc8142b12362ac0d09eedd05a93b715ec5951
Spectatori <cavdar2002@gmail.com>
2024-11-05 01:31:41 +0200

    feat: navbar and footer are now in default.vue so it's across all pages

---
+++ apps/tasky/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="flex flex-col justify-between">
+      <NavbarComponent/>
+      <div class="flex ">
+        <slot/>
+      </div>
+      <FooterComponent/>
+    </div>
+  </template>
+  
+  <script lang="ts" setup>
+  
+  </script>
+  
+  <style scoped>
+  </style>
