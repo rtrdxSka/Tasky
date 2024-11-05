36d62eeeda968da3cfc4b37a1b3b713ffa015d9f
Spectatori <cavdar2002@gmail.com>
2024-11-05 15:46:39 +0200

    feat: create about page and add colors for it in tailwind.config

---
+++ apps/tasky/pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="flex flex-col">
+        <img src="../public/aboutImages/sakura.webp" class="absolute z-0">
+        <div class="bg-about-50/50 flex flex-col w-screen justify-between pb-20 z-10 rounded-3xl">
+            <h1 class="text-6xl pt-28 pl-20 text-about-200">
+                Who are we?
+            </h1>
+            <h2 class="text-4xl pt-16 pl-20 pr-40 text-about-200">
+                At Tasky, we're on a mission to make the world greener, one task at a time.
+                We believe that positive change happens through small, meaningful actions.
+                That's why Tasky combines productivity with environmental impact: 
+                every time you complete a task, we plant a tree on your behalf. 
+                It’s our way of turning everyday goals into real-life growth.
+                With each task you accomplish, you're not just checking off an item on your 
+                to-do list—you’re contributing to global reforestation efforts. 
+                Whether it's a simple daily habit or a big project, your efforts help create a 
+                greener, healthier planet. Join us in making productivity sustainable. Plant trees. 
+                Achieve goals. Grow together with Tasky.
+            </h2>
+        </div>
+        <div class="bg-about-50/50 flex flex-col w-screen justify-between pb-44 z-10 rounded-3xl">
+            <h1 class="text-6xl pt-28 pl-20 text-about-200">
+                Why did our team choose to create Tasky?
+            </h1>
+            <h2 class="text-4xl pt-16 pl-20 pr-40 text-about-200">
+                In today’s fast-paced world, it’s easy to get caught up in to-do lists and 
+                daily demands without feeling connected to a larger purpose. 
+                We created Tasky to bridge that gap—to give productivity a purpose. With each task completed, 
+                users contribute to a cause much bigger than themselves: a greener planet.
+                We saw an opportunity to inspire people to make their personal growth journey impactful on a global scale. 
+                By transforming daily tasks into planted trees, Tasky empowers users to be part of the 
+                solution to climate change, all while staying motivated to reach their goals. 
+                Tasky is here to remind everyone that productivity and purpose can 
+                go hand in hand, and that even small actions can make a big difference.
+            </h2>
+        </div>
+    </div>
+</template>
