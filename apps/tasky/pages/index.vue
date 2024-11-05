74bf157bd522e5edaf42d9861551260012b456ea
Spectatori <cavdar2002@gmail.com>
2024-11-05 16:45:41 +0200

    style: change text color of text in the boxes below "Why Tasky?" and added some padding top to the 2 main divs so it looks better

--- apps/tasky/pages/index.vue
+++ apps/tasky/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-between bg-index-150/20">
     <img src="../public/Unlogged/mainPictures/mainFlower.png" class="absolute z-0">
-    <div class="flex w-screen justify-between pb-64 z-10 bg-index-50/60 rounded-3xl">
+    <div class="flex w-screen justify-between pb-64 pt-24 z-10 bg-index-50/60 rounded-3xl">
       <div class="z-10 pl-48 flex flex-col gap-12">
         <h1 class="text-6xl pt-24 text-index-300">Tasky</h1>
         <h2 class="w-96 text-4xl text-index-300"> Organize and complete daily tasks while contributing to a greener planet.
@@ -12,20 +12,20 @@
         <UtilsCarousel />
       </div>
     </div>
-    <div class="flex justify-between pb-32 pt-36">
+    <div class="flex justify-between pb-32 pt-44">
       <div class="z-10 flex flex-col pl-32 gap-20 ">
         <h1 class="text-6xl  text-index-250 pb-4">Why Tasky?</h1>
         <div class="bg-index-200 w-96 rounded-xl flex items-center">
           <img src="../public/Unlogged/mainPictures/note.png" class="p-6">
-          <h2 class="text-xl text-index-50 text-center pr-4">Tasky helps you organize your day to day life.</h2>
+          <h2 class="text-xl text-center pr-4">Tasky helps you organize your day to day life.</h2>
         </div>
         <div class="bg-index-200 w-96 rounded-xl flex items-center">
           <img src="../public/Unlogged/mainPictures/plant.png" class="p-6">
-          <h2 class="text-xl text-index-50 text-center pr-4">You save the planet by using our app.</h2>
+          <h2 class="text-xl text-center pr-4">You save the planet by using our app.</h2>
         </div>
         <div class="bg-index-200 w-96 rounded-xl flex items-center">
           <img src="../public/Unlogged/mainPictures/www.png" class="p-6">
-          <h2 class="text-xl text-index-50 text-center pr-4">Cross-platform accessability.</h2>
+          <h2 class="text-xl text-center pr-4">Cross-platform accessability.</h2>
         </div>
       </div>
       <div class="flex flex-col">
