8c613622ea9d2a0d2d2a16980c14da867fd3d34a
Spectatori <cavdar2002@gmail.com>
2024-11-05 14:54:18 +0200

    feat: added paths to navbar

--- apps/tasky/components/NavbarComponent.vue
+++ apps/tasky/components/NavbarComponent.vue
@@ -1,22 +1,32 @@
 <template>
-    <div  :class="['flex justify-between border-b-2', indexNavbar, ]"  >
+    <div :class="['flex justify-between border-b-2', indexNavbar,]">
         <div class="flex pl-10">
-            <img src="../public/navbarPictures/Tasky.jpg" class="w-20 p-4 transition ease-in-out delay-150 duration-300 hover:scale-110" style="border-radius:32px">
+            <a href="/">
+                <img src="../public/navbarPictures/Tasky.jpg" class="w-20 p-4 transition ease-in-out delay-150 duration-300 hover:scale-110" style="border-radius:32px">
+            </a>
             <button :class="['text-3xl self-center text-primary-200 font-roboto', indexDefaultText]">Tasky</button>
         </div>
         <div class="text-lg py-3 pr-4">
-            <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
-                Features
-            </button>
-            <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
-                Getting started
-            </button>
-            <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
-                About
-            </button>
-            <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
-                Login
-            </button>
+            <a href="/features">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                    Features
+                </button>
+            </a>
+            <a href="/getting-started">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                    Getting started
+                </button>
+            </a>
+            <a href="/about">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                    About
+                </button>
+            </a>
+            <a href="/access/login">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                    Login
+                </button>
+            </a>
         </div>
     </div>
 </template>
@@ -28,18 +38,18 @@ import { useRoute } from 'vue-router';
 const route = useRoute();
 
 const indexNavbar = computed(() => {
-  return route.name === 'index'
-    ? 'border-primary-150 bg-primary-50'
-    : '';
+    return route.name === 'index'
+        ? 'border-primary-150 bg-primary-50'
+        : '';
 });
-const indexNavbarButtons = computed(()=>{
+const indexNavbarButtons = computed(() => {
     return route.name === 'index'
-    ? `${indexDefaultText.value} hover:text-primary-100 hover:bg-primary-150`
-    : '';
+        ? `${indexDefaultText.value} hover:text-primary-100 hover:bg-primary-150`
+        : '';
 })
-const indexDefaultText = computed(()=>{
+const indexDefaultText = computed(() => {
     return route.name === 'index'
-    ? 'text-primary-200'
-    : '';
+        ? 'text-primary-200'
+        : '';
 })
 </script>
