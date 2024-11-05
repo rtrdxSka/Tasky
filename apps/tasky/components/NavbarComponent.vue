f9fcd85d11ea3294980174e88a4b96ab99158c68
Spectatori <cavdar2002@gmail.com>
2024-11-05 15:59:27 +0200

    feat: add navbar recognition of about page

--- apps/tasky/components/NavbarComponent.vue
+++ apps/tasky/components/NavbarComponent.vue
@@ -1,29 +1,29 @@
 <template>
-    <div :class="['flex justify-between border-b-2', indexNavbar,]">
+    <div :class="['flex justify-between border-b-2 ', indexNavbar, aboutNavbar]">
         <div class="flex pl-10">
             <a href="/">
                 <img src="../public/navbarPictures/Tasky.jpg" class="w-20 p-4 transition ease-in-out delay-150 duration-300 hover:scale-110" style="border-radius:32px">
             </a>
-            <button :class="['text-3xl self-center font-roboto', indexDefaultText]">Tasky</button>
+            <button :class="['text-3xl self-center font-roboto text-about-150', indexDefaultText, aboutDefaultText]">Tasky</button>
         </div>
         <div class="text-lg py-3 pr-4">
             <a href="/features">
-                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons, aboutNavbarButtons]">
                     Features
                 </button>
             </a>
             <a href="/getting-started">
-                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons, aboutNavbarButtons]">
                     Getting started
                 </button>
             </a>
             <a href="/about">
-                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons, aboutNavbarButtons]">
                     About
                 </button>
             </a>
             <a href="/access/login">
-                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons]">
+                <button :class="['p-4 hover:rounded-xl', indexNavbarButtons, aboutNavbarButtons]">
                     Login
                 </button>
             </a>
@@ -51,5 +51,21 @@ const indexDefaultText = computed(() => {
     return route.name === 'index'
         ? 'text-index-250'
         : '';
+})
+
+const aboutNavbar = computed (()=>{
+    return route.name === 'about'
+    ? 'border-about-150 bg-about-50'
+    : ''
+})
+const aboutNavbarButtons = computed(()=>{
+    return route.name === 'about'
+    ? `${aboutDefaultText.value} hover:text-about-50 hover:bg-about-150`
+    : ''
+})
+const aboutDefaultText = computed(()=>{
+    return route.name === 'about'
+    ? 'text-about-150'
+    : ''
 })
 </script>
