6222056604e56f329393f540055c128075dd9dd6
Spectatori <cavdar2002@gmail.com>
2024-11-13 15:59:53 +0200

    feat: removed unused class values from carousel

--- apps/tasky/components/Utils/Carousel.vue
+++ apps/tasky/components/Utils/Carousel.vue
@@ -1,53 +1,27 @@
 <template>
-  <n-carousel
-    effect="card"
-    prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
-    next-slide-style="transform: translateX(50%) translateZ(-800px);"
-    class="h-96 pr-24 pt-10"
-    style="width:60rem"
-    :show-dots="false"
-  >
+  <n-carousel 
+    effect="card" prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
+    next-slide-style="transform: translateX(50%) translateZ(-800px);" class="h-96 pt-10" :show-dots="false">
     <n-carousel-item :style="{ width: '60%' }">
-      <img
-        class="carousel-img rounded-xl"
-        src="../../public/CarouselImages/Carousel1.webp"
-      >
+      <img class="carousel-img rounded-xl" src="../../public/CarouselImages/Carousel1.webp">
     </n-carousel-item>
     <n-carousel-item :style="{ width: '60%' }">
-      <img
-        class="carousel-img rounded-xl"
-        src="../../public/CarouselImages/Carousel2.webp"
-      >
+      <img class="carousel-img rounded-xl" src="../../public/CarouselImages/Carousel2.webp">
     </n-carousel-item>
     <n-carousel-item :style="{ width: '60%' }">
-      <img
-        class="carousel-img rounded-xl"
-        src="../../public/CarouselImages/Carousel3.webp"
-      >
+      <img class="carousel-img rounded-xl" src="../../public/CarouselImages/Carousel3.webp">
     </n-carousel-item>
     <n-carousel-item :style="{ width: '60%' }">
-      <img
-        class="carousel-img rounded-xl"
-        src="../../public/CarouselImages/Carousel4.webp"
-      >
+      <img class="carousel-img rounded-xl" src="../../public/CarouselImages/Carousel4.webp">
     </n-carousel-item>
     <n-carousel-item :style="{ width: '60%' }">
-      <img
-        class="carousel-img rounded-xl"
-        src="../../public/CarouselImages/Carousel5.webp"
-      >
+      <img class="carousel-img rounded-xl" src="../../public/CarouselImages/Carousel5.webp">
     </n-carousel-item>
     <n-carousel-item :style="{ width: '60%' }">
-      <img
-        class="carousel-img rounded-xl"
-        src="../../public/CarouselImages/Carousel6.webp"
-      >
+      <img class="carousel-img rounded-xl" src="../../public/CarouselImages/Carousel6.webp">
     </n-carousel-item>
     <n-carousel-item :style="{ width: '60%' }">
-      <img
-        class="carousel-img rounded-xl"
-        src="../../public/CarouselImages/Carousel7.webp"
-      >
+      <img class="carousel-img rounded-xl" src="../../public/CarouselImages/Carousel7.webp">
     </n-carousel-item>
   </n-carousel>
 </template>
@@ -56,7 +30,6 @@
 .carousel-img {
   margin: 0 auto;
   width: 100%;
-  height: 100%;
-  object-fit: cover;
+  height: 100%; object-fit: cover;
 }
 </style>
