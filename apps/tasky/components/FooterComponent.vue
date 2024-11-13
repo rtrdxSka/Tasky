584acca64fba628e72b559f63d5adb0ccc6bb0dc
Spectatori <cavdar2002@gmail.com>
2024-11-13 16:13:23 +0200

    feat: add responsiveness to footer

--- apps/tasky/components/FooterComponent.vue
+++ apps/tasky/components/FooterComponent.vue
@@ -1,19 +1,19 @@
 <template>
-    <div :class="['flex  h-28 w-full border-t-2 justify-between z-10', indexFooter, aboutFooter, gettingStartedFooter, featuresFooter]">
-        <div class="flex pt-3 pl-4 gap-2">
-            <img src="../public/navbarPictures/Tasky.jpg" class="w-20 h-20 p-4 self-center" style="border-radius:32px">
-            <div class="flex flex-col pl-10 ">
+    <div :class="['flex  h-28 w-full border-t-2 justify-between z-10 max-xl:flex-col max-xl:items-center max-xl:h-full', indexFooter, aboutFooter, gettingStartedFooter, featuresFooter]">
+        <div class="flex pt-3 pl-4 gap-2 max-xl:gap-10 max-xl:px-5">
+            <img src="../public/navbarPictures/Tasky.jpg" class="w-20 h-20 p-4 self-center max-xl:hidden" style="border-radius:32px">
+            <div class="flex flex-col pl-10 max-xl:p-0">
                 <p style="font-size:15px">Useful Information</p>
                 <p :class="['text-xs hover:cursor-pointer pt-1', indexFooterText, aboutFooterText, gettingStartedFooterText, featuresFooterText]">Support</p>
                 <p :class="['text-xs hover:cursor-pointer', indexFooterText, aboutFooterText, gettingStartedFooterText, featuresFooterText]">Often asked questions</p>
             </div>
-            <div class="flex flex-col pl-10">
+            <div class="flex flex-col pl-10 max-xl:p-0">
                 <p style="font-size:15px">About us</p>
                 <p :class="['text-xs hover:cursor-pointer pt-1', indexFooterText, aboutFooterText, gettingStartedFooterText, featuresFooterText]">About Tasky</p>
                 <p :class="['text-xs hover:cursor-pointer', indexFooterText, aboutFooterText, gettingStartedFooterText, featuresFooterText]">Careers</p>
                 <p :class="['text-xs hover:cursor-pointer', indexFooterText, aboutFooterText, gettingStartedFooterText, featuresFooterText]">Privacy & Policy</p>
             </div>
-            <div class="flex flex-col pl-10">
+            <div class="flex flex-col pl-10 max-xl:p-0">
                 <p style="font-size:15px">Contacts</p>
                 <p class="text-xs text-neutral-500 pt-1">Monday - Friday</p>
                 <p class="text-xs text-neutral-500">08 AM - 19 PM UTC+02:00</p>
@@ -21,7 +21,7 @@
                 <p class="text-xs text-neutral-500">tasky@gmail.com</p>
             </div>
         </div>
-        <div class="flex pr-8 items-center gap-4">
+        <div class="flex pr-8 items-center gap-4 max-xl:py-4">
             <a href="google.com">
                 <img src="../public/footerPictures/linkedin.png"></a>
             <a href="google.com">
