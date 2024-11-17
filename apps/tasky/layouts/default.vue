ddb38dfb181755c4bdb27668aa1fe0d094d891d5
Spectatori <cavdar2002@gmail.com>
2024-11-18 00:51:27 +0200

    feat: create responsive login page

--- apps/tasky/layouts/default.vue
+++ apps/tasky/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col justify-between">
+    <div class="flex flex-col justify-between min-h-screen">
       <NavbarComponent/>
-      <div class="flex ">
+      <div class="flex">
         <slot/>
       </div>
       <FooterComponent/>
